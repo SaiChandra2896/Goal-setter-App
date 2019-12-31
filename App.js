6d2857d946f0af49c3b1754cf2d4d8c10f7d1549
SaiChandra2896 <saichandra2896@gmail.com>
2019-12-31 12:34:19 +0530

    add text component and button

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{ padding: 50 }}>
+      <View>
+        <TextInput placeholder='Add goal' style={{ borderBottomColor: 'black', borderWidth: 1, padding: 10 }} />
+        <Button title='Add' />
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
